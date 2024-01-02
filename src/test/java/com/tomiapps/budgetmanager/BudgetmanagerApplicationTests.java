1335a82ec6883cde873c99d298076c399110bfd0
Süte Tamás <tsute@szintezis-net.hu>
2024-01-02 13:03:36 +0100

    initial commit

---
+++ src/test/java/com/tomiapps/budgetmanager/BudgetmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomiapps.budgetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
